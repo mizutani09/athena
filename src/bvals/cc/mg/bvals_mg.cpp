acfc119c7a381690d242c33238e18b5b3a468cfc
mizutani <mizutani614@gmail.com>
2026-07-06 14:25:38 +0900

    Fix MPI_Request debug logging type in bvals_mg.cpp

--- src/bvals/cc/mg/bvals_mg.cpp
+++ src/bvals/cc/mg/bvals_mg.cpp
@@ -975,8 +975,9 @@ bool MGBoundaryValues::ReceiveMultigridBoundaryBuffers(BoundaryQuantity type,
   bool bflag = true;
   int r; MPI_Comm_rank(MPI_COMM_WORLD, &r);
   auto log_comm = [&](const char* where, int e, int peer, int tag, int cnt, MPI_Request req){
-    fprintf(stderr,"[r%02d] %s e=%d peer=%d tag=%d cnt=%d req=%p t=%.6f\n",
-            r, where, e, peer, tag, cnt, (void*)req, MPI_Wtime());
+    MPI_Fint req_f = MPI_Request_c2f(req);
+    fprintf(stderr,"[r%02d] %s e=%d peer=%d tag=%d cnt=%d req_f=%ld t=%.6f\n",
+            r, where, e, peer, tag, cnt, static_cast<long>(req_f), MPI_Wtime());
   };
 
   // log_comm("ReceiveMultigridBoundaryBuffers", 1, 0, 0, 0, MPI_REQUEST_NULL);
